30b1c5929348e8807e941b3fdb8c823535879d5d
amitabhdas96 <amitabh.sweden@gmail.com>
2024-04-15 20:35:08 +1200

    okok2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const app = express()
+app.use('/', express.static('public'))
+
 const port = 3000
 app.get('/', (req, res) => {
- res.send('Hello World!')
+ res.send('Hello Bunty 55!')
 })
 app.listen(port, () => {
  console.log(`Example app listening at http://localhost:${port}`)
 })
+
+// const express = require('express')
+// const ports = [9004, 9002, 9003]
+// ports.forEach((port) => {
+
+//     const app = express()
+
+//     app.use('/', express.static('public'))
+  
+//     app.get('/', (req, res) => {
+//      res.send('Hello Bunty 55!')
+//     })
+
+// app.listen(port, () => {
+//  console.log(`Example app listening at http://localhost:${port}`)
+// })
+// });
